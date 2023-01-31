c6e70fef6d3eb3a7f146acd3258daf313724f318
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-30 17:22:55 -0800

    contact.js sanity schema created and imported into index.js

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,5 +1,6 @@
 import testimonials from "./testimonials"
 import abouts from "./abouts"
 import brands from "./brands"
+import contact from "./contact"
 
-export const schemaTypes = [testimonials, abouts, brands]
+export const schemaTypes = [testimonials, abouts, brands, contact]
