da2c278feca6a8248a776f2b4e342eac61bd7034
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-31 12:25:19 -0800

    assets images uploaded with logos and exported default as images from constants and imported into Navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
 import './Navbar.scss';
+import { images } from '../../constants';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div>
+        <img src={images.Min192} alt='logo' />
+      </div>
+      <ul>
+        {['home', 'about', 'contact', 'work', 'skills', 'testimonials', 'footer'].map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>
+              {item}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
