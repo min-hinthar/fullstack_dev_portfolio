2367dae80058fb30f28355c3a54424b3dd19a5a3
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-30 17:24:56 -0800

    skills.js sanity schema created and imported into index.js

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -3,5 +3,6 @@ import abouts from "./abouts"
 import brands from "./brands"
 import contact from "./contact"
 import experiences from "./experiences"
+import skills from "./skills"
 
-export const schemaTypes = [testimonials, abouts, brands, contact, experiences]
+export const schemaTypes = [testimonials, abouts, brands, contact, experiences, skills]
