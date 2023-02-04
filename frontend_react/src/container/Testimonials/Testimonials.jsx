f85950ba95304116228e652ed5453fba00ec1ba0
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-03 17:46:45 -0800

    Testimonials.jsx updated with useState for testimonials, brands and currentIndex, useEffect to fetch data, MotionWrap and AppWrap component

--- frontend_react/src/container/Testimonials/Testimonials.jsx
+++ frontend_react/src/container/Testimonials/Testimonials.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client, urlFor } from '../../client';
 import './Testimonials.scss';
 
 const Testimonials = () => {
+
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query)
+      .then((data) => {
+        setTestimonials(data);
+      })
+    client.fetch(brandsQuery)
+      .then((data) => {
+        setBrands(data);
+      })
+  }, [])
+
   return (
     <div>Testimonials</div>
   )
 }
 
-export default Testimonials;
+export default AppWrap(
+  MotionWrap(Testimonials, 'app__testimonials'),
+  'testimonials'
+  );
