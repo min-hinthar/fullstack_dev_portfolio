1330daa45e01beab6d7591f355604579b64c7768
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-03 17:34:48 -0800

    MotionWrap.jsx created as Higher Order functional component exported as MotionWrap and rendered on About, Work and Skills components

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { client, urlFor } from '../../client';
 
 import './Skills.scss';
@@ -98,4 +98,7 @@ const Skills = () => {
   )
 }
 
-export default AppWrap(Skills, 'skills');
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'),
+  'skills'
+  );
