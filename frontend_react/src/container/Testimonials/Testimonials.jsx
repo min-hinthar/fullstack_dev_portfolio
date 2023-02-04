25971f1223a931631e8ac9ec736c732a22ee8d6f
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-03 21:23:56 -0800

    Testimonials.scss styling updated with proper div and image sizing for testimonials and brand logos, filter(grayscale), hover effect, shadow-boxing, linear gradient background coloring

--- frontend_react/src/container/Testimonials/Testimonials.jsx
+++ frontend_react/src/container/Testimonials/Testimonials.jsx
@@ -64,7 +64,7 @@ const Testimonials = () => {
         </>
       )}
     {/*  */}
-      <div className='app__testimonials-brands app__flex'>
+      <div className='app__testimonial-brands app__flex'>
         {brands.map((brand) => (
           <motion.div
             whileInView={{ opacity: [0, 1] }}
